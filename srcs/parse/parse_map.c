4705cce5e2900c34e0d7baccb3b52d4920eac83c
alsmd <dangerveneta@gmail.com>
2022-03-23 11:58:14 -0300

    is_surrounded by wall

--- srcs/parse/parse_map.c
+++ srcs/parse/parse_map.c
@@ -1,5 +1,73 @@
 #include <cub3d.h>
 
+
+int	valid_direction(t_game *game, int x, int y)
+{
+	if (x < 0 || y < 0)
+		return (0);
+	if (game->walk[y] == 0 || game->walk[y][x] == -1)
+		return (0);
+	if (game->map[y][x] != WALL)
+		return (0);
+	return (1);
+}
+
+int	check_surrounded(char **map, int x, int y)
+{
+	int	tmp_x;
+	int	tmp_y;
+
+	tmp_x = x;
+	tmp_y = y;
+	while (map[y][x] && map[y][x] != WALL)
+		x++;
+	if (map[y][x] != WALL)
+		return (0);
+	x = tmp_x;
+	while (x >= 0 && map[y][x] != WALL)
+		x--;
+	if (x < 0)
+		x = 0;
+	if (map[y][x] != WALL)
+		return (0);
+	x = tmp_x;
+	while (map[y] && map[y][x] != WALL)
+		y++;
+	if (map[y][x] != WALL)
+		return (0);
+	y = tmp_y;
+	while (y >= 0 && map[y][x] != WALL)
+		y--;
+	if (y < 0)
+		y = 0;
+	if (map[y][x] != WALL)
+		return (0);
+	return (1);
+}
+
+int	is_surrounded(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			if (ft_char_in_set(game->map[y][x], "ESWN02"))
+			{
+				if (!check_surrounded(game->map, x, y))
+					return (0);
+			}
+			x++;
+		}
+		y++;
+	}
+	return (1);
+}
+
 int	parse_map(t_game *game, char *file)
 {
 	int fd;
@@ -38,6 +106,8 @@ int	parse_map(t_game *game, char *file)
 		line = get_next_line(fd);
 	}
 	game->map = ft_split(map_str, '\n');
+	if (!is_surrounded(game))
+		return (1);
 	close(fd);
 	free(map_str);
 	return (0);
