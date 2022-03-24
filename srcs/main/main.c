ebda15a399f67eda723b662499da6cec52f29bf2
gsilva-v <gaa.silva53@gmail.com>
2022-03-24 11:38:28 -0300

    find seg

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -4,6 +4,11 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 
+	if (argc != 2)
+	{
+		printf("Error: invalid arguments, try ./cub3d ./maps/simple_map.cub\n");
+		exit(1);
+	}
 	ft_bzero(&game, sizeof(game));
 	if (parse_resources(&game, argv[1]))
 		return (1);
